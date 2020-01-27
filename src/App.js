3879654570d4c8bc49461db4c5a76dee977515b7
Wai Min <leopard23@gmail.com>
2020-01-27 11:52:16 +0800

    Add 27 cards using map

--- src/App.js
+++ src/App.js
@@ -3,8 +3,45 @@ import "./App.css";
 import Card from "./components/Card";
 
 class App extends Component {
+  state = {
+    numbers: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27
+    ]
+  };
+
+  renderGrid = () => {
+    return this.state.numbers.map(num => {
+      return <Card number={num} />;
+    });
+  };
+
   render() {
-    return <Card number={11} />;
+    return <div>{this.renderGrid()}</div>;
   }
 }
 
