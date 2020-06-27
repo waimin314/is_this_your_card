b98bd0ba3ff0ff5ce41a3b5dd48fefe2eafdc933
Wai Min <leopard23@gmail.com>
2020-06-27 09:21:23 +0800

    Updates

---
+++ precache-manifest.245314818b9df82ae794fce4fe6fb87b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94600e3ff608870abc6b3e0e87f81b4",
+    "url": "/is_this_your_card/index.html"
+  },
+  {
+    "revision": "30a036a48e4adcbe295a",
+    "url": "/is_this_your_card/static/css/main.9255b23c.chunk.css"
+  },
+  {
+    "revision": "e1b4993351fb0ef37752",
+    "url": "/is_this_your_card/static/js/2.b3bb00c1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/is_this_your_card/static/js/2.b3bb00c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30a036a48e4adcbe295a",
+    "url": "/is_this_your_card/static/js/main.8323e136.chunk.js"
+  },
+  {
+    "revision": "49706d26d9b82953d43c",
+    "url": "/is_this_your_card/static/js/runtime-main.aca4b25a.js"
+  }
+]);
